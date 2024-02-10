0e7bce6a4ed3ff3fc680e0a00e9355859bb1f8f4
Thithira-Paranawithana <153026117+Thithira-Paranawithana@users.noreply.github.com>
2024-02-10 15:22:57 +0530

    Add files via upload

---
+++ CuddleCam_MobileApp/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\REDTECH\\AndroidStudioProjects\\cuddleslider" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=C:\\Users\\REDTECH\\AndroidStudioProjects\\cuddleslider"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\REDTECH\\AndroidStudioProjects\\cuddleslider\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\REDTECH\\AndroidStudioProjects\\cuddleslider"
+  "FLUTTER_TARGET=C:\\Users\\REDTECH\\AndroidStudioProjects\\cuddleslider\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9jZGJlZGE3ODhhMjkzZmEyOTY2NWRjM2ZhM2Q2ZTYzYmQyMjFjYjBkLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\REDTECH\\AndroidStudioProjects\\cuddleslider\\.dart_tool\\package_config.json"
+)
